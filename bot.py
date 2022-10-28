ee86c459ab035654845420295885c8930e150fd7
Chitranjan Gupta <37315561+chitranjan01@users.noreply.github.com>
2022-10-28 12:09:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ def Telegram():
 
   executor.start_polling(disp)
 
-async def Request(message: types.Message):
+async def Request(url):
     session = Session()
     session.headers.update({
         'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
