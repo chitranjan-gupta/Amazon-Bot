e88fbc90442f9f804e5c860c9769e0926c6ec57e
Chitranjan Gupta <37315561+chitranjan01@users.noreply.github.com>
2023-01-15 12:28:56 +0530

    Updated link sharing, BOT_TOKEN variable
    Improved sharing of amazon link to the bot and removed the confusion of BOT_TOKEN variable

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from re import sub, IGNORECASE
+from re import sub, IGNORECASE, findall
 from requests import Session
 from bs4 import BeautifulSoup
 from aiogram import Bot, Dispatcher, types, executor
@@ -6,11 +6,23 @@ from logging import basicConfig, INFO
 from os import environ
 import asyncio
 
-BOT_TOKEN = environ.get('token')  # Token used by telegram to authorize the use of bot; replace with your token
-TIME = 60 * 60  # Check the price every given seconds; change the time according to your need
-url = "https://www.amazon.in/OnePlus-Bluetooth-Wireless-Earphones-Bombastic/dp/B0B3MNYGTW"
+BOT_TOKEN = environ.get('BOT_TOKEN') # Token used by telegram to authorize the use of bot; replace with your token
+TIME = 60 * 60 # Check the price every given seconds; change the time according to your need
+url = "https://www.amazon.in/OnePlus-Bluetooth-Wireless-Earphones-Bombastic/dp/B0B3MNYGTW" #default link
 status = False # Status of tracker 
 prices = [] # array of price changes
+
+def CheckUrl(text,type):
+  reg = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+  url = findall(reg,text)
+  if type == "c":
+    if(url):
+      return True
+    else:
+      return False
+  else:
+    if(url):
+      return url[0][0]
 
 def Telegram():
   bot = Bot(token=BOT_TOKEN)
@@ -19,8 +31,7 @@ def Telegram():
   @disp.message_handler(commands=['start'])
   async def send_welcome(message: types.Message):
     await message.answer(
-      f"""Hi! {message['from']['first_name']} \n Welcome You to Amazon Product Price Tracker Bot \n 
-    You can track the price of Amazon Product by sending the link.\n You can send the link using the command /link url \n"""
+      f"""Hi! {message['from']['first_name']}\nWelcome You to Amazon Product Price Tracker Bot. You can track the price of Amazon Product by sending the link.\n"""
     )
 
   @disp.message_handler(commands=['tstart'])
@@ -43,22 +54,20 @@ def Telegram():
     else:
       await message.answer("Tracking is already stopped.")
 
-  @disp.message_handler(commands=['link'])
-  async def send_link(message: types.Message):
-    global url, prices
-    if status == False:
-      url = message['text'].replace("/link", "")
+  @disp.message_handler(commands=['help'])
+  async def send_help(message: types.Message):
+    await message.reply("Type /start to Interact With The Bot")
+  
+  @disp.message_handler()
+  async def check(message: types.Message):
+    global status, url, prices
+    if status == False and CheckUrl(message['text'],"c"):
+      url = CheckUrl(message['text'],"n")
       prices.clear()
       await message.answer(
         "Link is Added to tracking\n To Start Tracking send command /tstart \nand to Stop Tracking send command /tstop"
       )
-    else:
-      await message.answer("Tracking is already running. Stop The Tracker and change the link")
-
-  @disp.message_handler(commands=['help'])
-  async def send_help(message: types.Message):
-    await message.reply("Type /start to Interact With The Bot")
-
+  
   executor.start_polling(disp)
 
 async def Request(url):
@@ -86,10 +95,10 @@ async def Check(message: types.Message):
     if len(prices) >= 1:
       if currentprice < prices[-1]:
         prices.append(currentprice)
-        await message.answer("Current Price: " + str(currentprice))
+        await message.answer("Current Price: ₹" + str(currentprice))
     else:
       prices.append(currentprice)
-      await message.answer("Current Price: " + str(currentprice))
+      await message.answer("Current Price: ₹" + str(currentprice))
     await asyncio.sleep(TIME)
 
 if __name__ == '__main__':
